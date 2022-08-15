636a7666a383fc96143ecbd636000133e73fad6e
Hyeon Jeon <hj@hcil.snu.ac.kr>
2022-08-15 23:56:27 +0900

    added skeletal code for merge tree update

--- cambiguity/merge_tree.py
+++ cambiguity/merge_tree.py
@@ -1,3 +1,4 @@
+from operator import is_
 import numpy as np
 from numba import njit
 
@@ -9,6 +10,20 @@ def construct_value_indices(grid, values, x_idx, y_idx, size):
 			values[i * size + j] = grid[i, j]
 			x_idx[i * size + j] = i
 			y_idx[i * size + j] = j
+
+
+def is_adjacent_cells_occupied(node_distribution, x, y):
+	"""
+	check if the adjacent cells of (x,y) are occupied
+	"""
+	for i in range(max(x - 1, 0), min(x + 2, len(node_distribution))):
+		for j in range(max(y - 1, 0), min(y + 2, len(node_distribution))):
+			if i == x and j == y:
+				continue
+			if node_distribution[i, j] != -1:
+				return True
+	return False
+	
 
 def construct_merge_tree(grid):
 	"""
@@ -28,7 +43,34 @@ def construct_merge_tree(grid):
 	x_idx = x_idx[sort_indices]
 	y_idx = y_idx[sort_indices]
 
-	## 
+	## traverse the sorted arrays and construct the merge tree
+	node_distribution = np.full((size, size), -1)
+	node_pool         = {}
+
+	curr_F = values[0]
+	for i in range(size ** 2):
+
+		## confirm the constructed merge tree candidate 
+		if val != curr_F:
+			## TODO
+			## TODO
+			curr_F = val
+			
+		## update the merge tree candidate
+		x, y, val = x_idx[i], y_idx[i], values[i]
+		if is_adjacent_cells_occupied(node_distribution, x, y):
+			##TODO
+			pass
+		else:
+			##TODO
+			pass
+
+
+
+
+		
+
+
 
 
 
