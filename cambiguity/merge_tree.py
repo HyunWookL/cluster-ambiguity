e320933c21ee2de5525b6115b91bcd9038ebd444
Hyeon Jeon <hj@hcil.snu.ac.kr>
2022-08-16 03:23:26 +0900

    implemented the first iteration for merge tree consturction

--- cambiguity/merge_tree.py
+++ cambiguity/merge_tree.py
@@ -1,79 +1,26 @@
-from operator import is_
-import numpy as np
-from numba import njit
+class MergeTree():
+	def __init__(self):
+		self.nodes = {}
+		self.curr_node_id = 0
 
-## function to construct the arrays
-@njit
-def construct_value_indices(grid, values, x_idx, y_idx, size):
-	for i in range(size):
-		for j in range(size):
-			values[i * size + j] = grid[i, j]
-			x_idx[i * size + j] = i
-			y_idx[i * size + j] = j
+	def initialize_node(self, val, x, y):
+		"""
+		initialize node with using node_id as key
+		returns the node_id of the initialized node
+		"""
+		self.nodes[self.curr_node_id] = {val: [(x, y)]} 
+		self.curr_node_id += 1
+		return self.curr_node_id - 1
 
 
-def is_adjacent_cells_occupied(node_distribution, x, y):
-	"""
-	check if the adjacent cells of (x,y) are occupied
-	"""
-	for i in range(max(x - 1, 0), min(x + 2, len(node_distribution))):
-		for j in range(max(y - 1, 0), min(y + 2, len(node_distribution))):
-			if i == x and j == y:
-				continue
-			if node_distribution[i, j] != -1:
-				return True
-	return False
-	
-
-def construct_merge_tree(grid):
-	"""
-	construct merge tree based on the given input grid
-	"""
-	size = len(grid)
-
-	## change grid into arrays holding the grid value and grid indices (x,y)
-	values = np.zeros(size ** 2)
-	x_idx  = np.zeros(size ** 2)
-	y_idx  = np.zeros(size ** 2)
-	construct_value_indices(grid, values, x_idx, y_idx, size)
-
-	## sort the arrays (values, x_idx, y_idx) based on the values
-	sort_indices = np.argsort(-values)
-	values = values[sort_indices]
-	x_idx = x_idx[sort_indices]
-	y_idx = y_idx[sort_indices]
-
-	## traverse the sorted arrays and construct the merge tree
-	node_distribution = np.full((size, size), -1)
-	node_pool         = {}
-
-	curr_F = values[0]
-	for i in range(size ** 2):
-
-		## confirm the constructed merge tree candidate 
-		if val != curr_F:
-			## TODO
-			## TODO
-			curr_F = val
-			
-		## update the merge tree candidate
-		x, y, val = x_idx[i], y_idx[i], values[i]
-		if is_adjacent_cells_occupied(node_distribution, x, y):
-			##TODO
-			pass
+	def add_cell(self, node_id, val, x, y):
+		"""
+		add coord to the node_id
+		raise KeyError if node_id does not exist
+		"""
+		if node_id not in self.nodes:
+			raise KeyError("node_id does not exist within the nodes set")
+		if val in self.nodes[node_id]:
+			self.nodes[node_id][val].append((x, y))
 		else:
-			##TODO
-			pass
-
-
-
-
-		
-
-
-
-
-
-
-
-	
+			self.nodes[node_id][val] = [(x, y)]
