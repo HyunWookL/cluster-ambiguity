4feaef8f3be36ff00a11bfd4be633761a7472e1e
Hyeon Jeon <hj@hcil.snu.ac.kr>
2022-08-19 01:30:40 +0900

    split visualize_gmm code

--- src/clustme_data/extract_gaussian.py
+++ src/clustme_data/extract_gaussian.py
@@ -2,42 +2,9 @@ from sklearn.mixture import GaussianMixture
 import reader as rd
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.patches import Ellipse
+
 import json
-
-"""
-Functions for drawing gaussian mixture model estimation result
-"""
-
-def draw_ellipse(position, covariance, ax=None, **kwargs):
-    """Draw an ellipse with a given position and covariance"""
-    ax = ax or plt.gca()
-    
-    # Convert covariance to principal axes
-    if covariance.shape == (2, 2):
-        U, s, Vt = np.linalg.svd(covariance)
-        angle = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
-        width, height = 2 * np.sqrt(s)
-    else:
-        angle = 0
-        width, height = 2 * np.sqrt(covariance)
-    
-    # Draw the Ellipse
-    for nsig in range(1, 4):
-        ax.add_patch(Ellipse(position, nsig * width, nsig * height,
-                             angle, **kwargs))
-
-def plot_gmm(gmm, X, labels, label=True, ax=None):
-    ax = ax or plt.gca()
-    if label:
-        ax.scatter(X[:, 0], X[:, 1], c=labels, s=3, cmap='viridis', zorder=2)
-    else:
-        ax.scatter(X[:, 0], X[:, 1], s=3, zorder=2)
-    ax.axis('equal')
-    
-    w_factor = 0.2 / gmm.weights_.max()
-    for pos, covar, w in zip(gmm.means_, gmm.covariances_, gmm.weights_):
-        draw_ellipse(pos, covar, alpha=w * w_factor)
+import visualize_gmm as vg
 
 """
 Functions for extracting gaussian infos from clustme
@@ -110,7 +77,7 @@ def extract(is_draw=False):
 		datum["data"] = scale_datum(datum["data"])
 		gmm, gaussian_info = extract_single(datum["data"])
 		if is_draw:
-			plot_gmm(gmm, datum["data"], gaussian_info["proba_labels"])
+			vg.plot_gmm(gmm, datum["data"], gaussian_info["proba_labels"])
 			plt.savefig("./plot_individual/" + str(i) + ".png")
 			# plt.savefig("./plot_individual_with_score/" + str(datum["prob_single"]) + ".png")
 			plt.clf()
