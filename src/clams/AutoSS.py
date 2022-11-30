6f2965e2ac9f76aed52609ed12f2a38da2626b8c
Hyeon Jeon <hj@hcil.snu.ac.kr>
2022-11-30 17:47:14 +0900

    added scatterplot sampling code

--- src/clams/AutoSS.py
+++ src/clams/AutoSS.py
@@ -1,3 +1,15 @@
 """
 Implementation of automatic scatterplot sampling (AutoSS) algorithm.
 """
+
+import numpy as np
+import CLAMS as clams
+from sampling.Sampler import *
+from sampling.SamplingMethods import *
+
+class AutoScatterplotSampling():
+	"""
+	Class for Automatic Scatterplot Sampling (AutoSS)
+	"""
+
+	
